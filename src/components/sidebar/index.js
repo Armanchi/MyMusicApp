bdd3f892c1955b984cc9c0bc9f82c5f800dae179
Armani Johnson <armanijohnson0104@gmail.com>
2023-02-03 09:53:35 -0600

    added auth login

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -1,16 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 import './sidebar.css'
-import SidebarButton from "./sidebarButton";
+import SidebarButton from './sidebarButton';
 import {MdFavorite} from "react-icons/md"
 import {FaGripfire, FaPlay} from "react-icons/fa"
 import {FaSignOutAlt} from "react-icons/fa"
 import {IoLibrary} from "react-icons/io5"
 import {MdSpaceDashboard} from "react-icons/md"
+import apiClient from '../../spotify';
 
 const Sidebar = () => {
+    const [image, setImage] = useState("https://i.pinimg.com/564x/03/52/69/035269c9cdd1f497bf86ed1fa0c54811.jpg");
+    useEffect(() => {
+        apiClient.get("me").then((response) => {
+            setImage(response.data.images[0].url);
+        })
+    }, []);
     return(
         <div className="sidebar-container">
-            <img src="https://i.pinimg.com/564x/03/52/69/035269c9cdd1f497bf86ed1fa0c54811.jpg" className="profile-img" alt="profile" />
+            <img src={image} className="profile-img" alt="profile" />
             <div>
                 <SidebarButton title="Feed" to="/feed" icon={<MdSpaceDashboard />}/>
                 <SidebarButton title="Trending" to="/trending" icon={<FaGripfire/>}/>
--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,14 +1,34 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Feed from "../feed/feed";
 import Library from "../library/library";
 import Trending from "../trending/trending";
 import Player from "../player/player";
 import Favorites from "../favorites/favorites";
-import './home.css'
 import Sidebar from "../../components/sidebar";
+import Login from "../auth/login";
+import {setClientToken} from "../../spotify";
+import './home.css'
+
 const Home = () => {
-    return(
+    const [token, setToken] = useState('');
+    useEffect(() => {
+    const token = window.localStorage.getItem('token');
+    const hash = window.location.hash;
+    window.location.hash = '';
+    if (!token && hash) {
+      const _token = hash.split('&')[0].split('=')[1];
+      window.localStorage.setItem('token', _token);
+      setToken(_token);
+      setClientToken(_token);
+    } else {
+      setToken(token);
+      setClientToken(token);
+    }
+  }, []);
+  return !token ? (
+    <Login />
+  ) : (
         <Router>
             <div className="main-body">
             <Sidebar />
