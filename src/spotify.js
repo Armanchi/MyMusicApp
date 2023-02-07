1b39ea46614b727c659910ad6a390568674bc462
Armani Johnson <armanijohnson0104@gmail.com>
2023-02-07 12:02:01 -0600

    updated player & queue folders

--- src/spotify.js
+++ src/spotify.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const authEndpoint = "https://accounts.spotify.com/authorize?";
 const clientId = "2c007a0ae62f46718856922bb6c9c3a7";
-const redirectUri = "http://localhost:3004";
+const redirectUri = "http://localhost:3002";
 const scopes = ["user-library-read", "playlist-read-private"];
 
 export const loginEndpoint = `${authEndpoint}client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
@@ -13,7 +13,6 @@ const apiClient = axios.create({
   baseURL: "https://api.spotify.com/v1/",
 });
 
-//save token as permanent header
 export const setClientToken = (token) => {
   apiClient.interceptors.request.use(async function (config) {
     config.headers.Authorization = "Bearer " + token;
